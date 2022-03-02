6d32c2870cf6e4e98924be12ccbf8046c4d7284a
NerVod01~ <benjamin.jeannerot@gmail.com>
2022-03-02 11:37:03 +0100

    render pug gagnants route highscore

--- serveur.js
+++ serveur.js
@@ -19,7 +19,8 @@ const app = express();
 console.log("résultats recherche ip objet :", objetIp);
 console.log('résultats recherche ip :', objetIp['results']['Wi-Fi'][0]);
 // console.log("résultats recherche ip :", objetIp["results"]["Ethernet 2"][0]);
-let socketServeur = objetIp['results']['Wi-Fi'][0]
+// let socketServeur = objetIp['results']['Wi-Fi'][0]
+let socketServeur = objetIp['results']['OpenVPN TAP-Windows6'][0]
 // let socketServeur = objetIp["results"]["Ethernet 2"][0];
 
 const config = {
@@ -58,18 +59,32 @@ app.get("/jeu", (req, res) => {
   }
 });
 
+
 app.get("/highscore", (req, res) => {
   Database.User.find({},{ _id:0, gamertag: 1, victories :1}).sort({victories: -1}).limit(10)
   .then((winners) => {
     console.log('log sur route highscore',winners)
+    hallOfFame.splice(0,9);
     for(let i=0; i< winners.length; i++){
       hallOfFame.push(winners[i])
       console.log('push hall of fame :', hallOfFame)
     }
-    
     console.log('hallOfFame après bouclage :', hallOfFame)
-  })
-  res.render("highscore.pug");
+  
+})
+
+  res.render("highscore.pug", {
+    premier: `${hallOfFame[0]}`, 
+    deuxieme: `${hallOfFame[1]}`, 
+    troisieme: `${hallOfFame[2]}`,
+    quatrieme: `${hallOfFame[3]}`,
+    cinquieme: `${hallOfFame[4]}`,
+    sixieme: `${hallOfFame[5]}`,
+    septieme: `${hallOfFame[6]}`,
+    huitieme: `${hallOfFame[7]}`,
+    neuvieme: `${hallOfFame[8]}`,
+    dixieme: `${hallOfFame[9]}`
+  });
 });
 
 app.get("*", (req, res) => {
